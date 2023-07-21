cd341e357fc3836e2546c7a3c7283cc2216d2181
clarionke <104459747+clarionke@users.noreply.github.com>
2023-07-21 03:29:39 +0300

    Creating Auth App

---
+++ authentication/urls.py
@@ -0,0 +1,9 @@
+from .views import RegistrationView
+from .views import UsernameValidationView
+from django.urls import path
+
+
+urlpatterns = [
+    path('register', RegistrationView.as_view(), name="register"),
+    path('validate-username', UsernameValidationView.as_view(),name="validate-username")
+    ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+#from django.http import response
 # Create your views here.
 
-def login(request):
-    return render(request, 'authentication/index.html')
+class UsernameValidationView(View):
+    def get(self,request):
+        data=json.loads(request.body)
+        username = data['username']
+        
+        if not str('username').isalnum():
+            return JsonResponse({'username_error':'Username should contain only alphanumeric charactors'})
+        return JsonResponse{('username_valid'),True}
+    
+class RegistrationView(View):
+    def get(self,request):
+        return render(request, 'authentication/register.html')
